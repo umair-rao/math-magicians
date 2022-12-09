3cb11d74959961e1738d1a9671aa022971363fb2
umair-rao <umairmuqeem@gmail.com>
2022-12-09 22:02:58 +0500

    Add Events

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,43 +1,54 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
 export default class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { total: 0, operation: null, next: null };
+    this.clickEvent = this.clickEvent.bind(this);
   }
 
+  clickEvent = (e) => {
+    this.setState((state) => calculate(state, e.target.textContent));
+  };
+
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="calculator-display">
-        <div className="result">0</div>
+        <div className="result">
+          {total}
+          {operation}
+          {next}
+        </div>
         <div className="row1">
-          <button type="button" className="cal-btn">AC</button>
-          <button type="button" className="cal-btn">+/-</button>
-          <button type="button" className="cal-btn">%</button>
-          <button type="button" className="orange-btn">+-</button>
+          <button type="button" className="cal-btn" onClick={this.clickEvent}>AC</button>
+          <button type="button" className="cal-btn" onClick={this.clickEvent}>+/-</button>
+          <button type="button" className="cal-btn" onClick={this.clickEvent}>%</button>
+          <button type="button" className="orange-btn" onClick={this.clickEvent}>+-</button>
         </div>
         <div className="row2">
-          <button type="button" className="cal-btn">7</button>
-          <button type="button" className="cal-btn">8</button>
-          <button type="button" className="cal-btn">9</button>
-          <button type="button" className="orange-btn">x</button>
+          <button type="button" className="cal-btn" onClick={this.clickEvent}>7</button>
+          <button type="button" className="cal-btn" onClick={this.clickEvent}>8</button>
+          <button type="button" className="cal-btn" onClick={this.clickEvent}>9</button>
+          <button type="button" className="orange-btn" onClick={this.clickEvent}>x</button>
         </div>
         <div className="row3">
-          <button type="button" className="cal-btn">4</button>
-          <button type="button" className="cal-btn">5</button>
-          <button type="button" className="cal-btn">6</button>
-          <button type="button" className="orange-btn">-</button>
+          <button type="button" className="cal-btn" onClick={this.clickEvent}>4</button>
+          <button type="button" className="cal-btn" onClick={this.clickEvent}>5</button>
+          <button type="button" className="cal-btn" onClick={this.clickEvent}>6</button>
+          <button type="button" className="orange-btn" onClick={this.clickEvent}>-</button>
         </div>
         <div className="row4">
-          <button type="button" className="cal-btn">1</button>
-          <button type="button" className="cal-btn">2</button>
-          <button type="button" className="cal-btn">3</button>
-          <button type="button" className="orange-btn">+</button>
+          <button type="button" className="cal-btn" onClick={this.clickEvent}>1</button>
+          <button type="button" className="cal-btn" onClick={this.clickEvent}>2</button>
+          <button type="button" className="cal-btn" onClick={this.clickEvent}>3</button>
+          <button type="button" className="orange-btn" onClick={this.clickEvent}>+</button>
         </div>
         <div className="row5">
-          <button type="button" className="zero-btn">0</button>
-          <button type="button" className="cal-btn">.</button>
-          <button type="button" className="orange-btn">=</button>
+          <button type="button" className="zero-btn" onClick={this.clickEvent}>0</button>
+          <button type="button" className="cal-btn" onClick={this.clickEvent}>.</button>
+          <button type="button" className="orange-btn" onClick={this.clickEvent}>=</button>
         </div>
       </div>
     );
